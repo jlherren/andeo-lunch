7b7ea957ef48b668d509fe13081fd67c402c14fa
Jean-Luc Herren <jlh@gmx.ch>
2021-05-01 12:51:02 +0200

    Bundle Roboto font directly

--- src/App.vue
+++ src/App.vue
@@ -107,6 +107,13 @@
 </script>
 
 <style lang="scss">
+    @import '~@fontsource/roboto/100.css';
+    @import '~@fontsource/roboto/300.css';
+    @import '~@fontsource/roboto/400.css';
+    @import '~@fontsource/roboto/500.css';
+    @import '~@fontsource/roboto/700.css';
+    @import '~@fontsource/roboto/900.css';
+
     .center-text {
         text-align: center;
     }
