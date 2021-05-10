d400ffdf16b574eccc34bea4bbaa4ba6e566929d
Jean-Luc Herren <jlh@gmx.ch>
2021-05-10 20:56:57 +0200

    Add SVG icon

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,31 @@ module.exports = {
     productionSourceMap: false,
 
     pwa: {
-        name:         'Lunch Money',
-        assetVersion: '1',
+        name:          'Lunch Money',
+        themeColor:    '#1e1617',
+        assetsVersion: '2',
+        iconPaths:     {
+            favicon16:      'img/icons/icon16.png',
+            favicon32:      'img/icons/icon32.png',
+            appleTouchIcon: 'img/icons/icon.svg',
+            maskIcon:       'img/icons/icon-maskable.svg',
+            msTileImage:    'img/icons/icon.svg',
+        },
+
+        manifestOptions: {
+            icons: [
+                {
+                    src:   'img/icons/icon.svg?v=1',
+                    sizes: 'any',
+                    type:  'image/svg+xml',
+                },
+                {
+                    src:     'img/icons/icon-maskable.svg?v=1',
+                    sizes:   'any',
+                    type:    'image/svg+xml',
+                    purpose: 'maskable',
+                },
+            ],
+        },
     },
 };
