bbfc5a7a542aef0d47f5218587fdd133762ddefb
Jean-Luc Herren <jlh@gmx.ch>
2021-07-04 21:40:37 +0200

    Take MariaDB parameters from environment if missing

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -21,6 +21,17 @@ exports.connect = async function connect(options) {
         ...options,
     };
 
+    // Use environment for missing config
+    if (options?.database === undefined) {
+        options.database = process.env.MARIADB_DATABASE;
+    }
+    if (options?.username === undefined) {
+        options.username = process.env.MARIADB_USER;
+    }
+    if (options?.password === undefined) {
+        options.password = process.env.MARIADB_PASSWORD;
+    }
+
     exports.sequelize = new Sequelize(options);
     await exports.sequelize.authenticate();
 
