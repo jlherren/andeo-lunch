7647151932a97af39c932694a4f7976a4f8f052c
Jean-Luc Herren <jlh@gmx.ch>
2021-08-05 16:31:42 +0200

    Backend: Attempt to reconnect to DB after a refused connection

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
-const {Sequelize} = require('sequelize');
+const {Sequelize, ConnectionRefusedError} = require('sequelize');
 const fs = require('fs').promises;
 
 const Models = require('./models');
 
 /** @type {Sequelize|null} */
 exports.sequelize = null;
+
+const MAX_CONNECTION_ATTEMPTS = 20;
 
 const ENV_FALLBACKS = {
     database: 'MARIADB_DATABASE',
@@ -48,7 +50,24 @@ exports.connect = async function connect(options) {
     }
 
     exports.sequelize = new Sequelize(options);
-    await exports.sequelize.authenticate();
+
+    for (let attempt = 0; attempt < MAX_CONNECTION_ATTEMPTS; attempt++) {
+        try {
+            await exports.sequelize.authenticate();
+        } catch (err) {
+            if (err instanceof ConnectionRefusedError) {
+                // Try again, the MariaDB docker image usually takes a while when started for the first time
+                console.error(`Failed to connect to the database (attempt ${attempt + 1}/${MAX_CONNECTION_ATTEMPTS}): ${err.message}`);
+                if (attempt + 1 >= MAX_CONNECTION_ATTEMPTS) {
+                    throw err;
+                }
+                console.log('Will try again in a bit...');
+                await new Promise(resolve => setTimeout(resolve, 5000));
+            } else {
+                throw err;
+            }
+        }
+    }
 
     Models.initModels(exports.sequelize);
 
