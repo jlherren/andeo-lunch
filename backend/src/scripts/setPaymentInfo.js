95c505c415bf21c2962a9a384b5e6d444f9dd25a
Jean-Luc Herren <jlh@gmx.ch>
2021-08-06 18:35:15 +0200

    Backend: Add script to set payment information

---
+++ backend/src/scripts/setPaymentInfo.js
@@ -0,0 +1,108 @@
+'use strict';
+
+const chalk = require('chalk');
+
+const Models = require('../db/models');
+const Cli = require('../cli');
+const LunchMoney = require('../lunchMoney');
+const ConfigProvider = require('../configProvider');
+const {sprintf} = require('sprintf-js');
+
+/**
+ * @returns {Promise<number|null>}
+ */
+async function fetchDefaultRecipient() {
+    let config = await Models.Configuration.findOne({where: {name: 'payUp.defaultRecipient'}});
+    return config ? parseInt(config.value, 10) : null;
+}
+
+/**
+ * @param {number} userId
+ * @returns {Promise<void>}
+ */
+async function saveDefaultRecipient(userId) {
+    await Models.Configuration.upsert({name: 'payUp.defaultRecipient', value: `${userId}`});
+}
+
+/**
+ * @param {number} userId
+ * @returns {Promise<string|null>}
+ */
+async function fetchPaymentInfo(userId) {
+    let config = await Models.Configuration.findOne({where: {name: `paymentInfo.${userId}`}});
+    return config?.value ?? null;
+}
+
+/**
+ * @param {number} userId
+ * @param {string} paymentInfo
+ * @returns {Promise<void>}
+ */
+async function savePaymentInfo(userId, paymentInfo) {
+    if (paymentInfo === '') {
+        await Models.Configuration.destroy({where: {name: `paymentInfo.${userId}`}});
+    } else {
+        await Models.Configuration.upsert({name: `paymentInfo.${userId}`, value: paymentInfo});
+    }
+}
+
+console.log(chalk.bold('Update payment information'));
+
+let lunchMoney = new LunchMoney({config: ConfigProvider.getMainConfig()});
+
+lunchMoney.sequelizePromise.then(async sequelize => {
+    let cli = new Cli();
+    try {
+        let defaultRecipient = await fetchDefaultRecipient();
+        let users = await Models.User.findAll({order: [['id', 'ASC']]});
+        console.log('\nID  R  Username          Display name');
+        for (let user of users) {
+            let line = sprintf(
+                '%-2d  %s  %-16s  %s',
+                user.id,
+                user.id === defaultRecipient ? '*' : ' ',
+                user.username,
+                user.name,
+            );
+            console.log(line);
+        }
+        console.log('');
+
+        let userId = parseInt(await cli.question('Select user: '), 10);
+        let user = users.find(u => u.id === userId);
+
+        if (!user) {
+            throw new Error('Invalid user');
+        }
+
+        let paymentInfo = await fetchPaymentInfo(userId);
+        console.log('');
+        if (paymentInfo !== null) {
+            console.log('Current payment info:');
+            console.log(paymentInfo);
+        } else {
+            console.log('No payment info currently set');
+        }
+        console.log('');
+
+        console.log('Enter new payment information, leave empty to not change, enter "-" to delete');
+        paymentInfo = await cli.question('New payment info: ');
+        if (paymentInfo !== '') {
+            if (paymentInfo === '-') {
+                paymentInfo = '';
+            }
+            await savePaymentInfo(userId, paymentInfo);
+            console.log('Payment info saved');
+        }
+
+        console.log('');
+        if (await cli.question('Set as default recipient? [y/N] ') === 'y') {
+            await saveDefaultRecipient(userId);
+            console.log('Successfully saved as default recipient');
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+    cli.close();
+    await sequelize.close();
+});
