7254e5339011e5c5253bd3f08235f428d90a2ca7
Jean-Luc Herren <jlh@gmx.ch>
2021-08-06 18:43:37 +0200

    Backend: Add script to edit user display name and password

---
+++ backend/src/scripts/editUser.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const chalk = require('chalk');
+
+const Models = require('../db/models');
+const Cli = require('../cli');
+const LunchMoney = require('../lunchMoney');
+const ConfigProvider = require('../configProvider');
+const {sprintf} = require('sprintf-js');
+const AuthUtils = require('../authUtils');
+
+console.log(chalk.bold('Edit user'));
+
+let lunchMoney = new LunchMoney({config: ConfigProvider.getMainConfig()});
+
+lunchMoney.sequelizePromise.then(async sequelize => {
+    let cli = new Cli();
+    try {
+        let users = await Models.User.findAll({order: [['id', 'ASC']]});
+        console.log('\nID  Username          Display name');
+        for (let user of users) {
+            console.log(sprintf('%-2d  %-16s  %s', user.id, user.username, user.name));
+        }
+        console.log('');
+
+        let userId = parseInt(await cli.question('Select user: '), 10);
+        let user = users.find(u => u.id === userId);
+
+        if (!user) {
+            throw new Error('Invalid user');
+        }
+
+        console.log('');
+        let name = await cli.question('New display name (leave blank to not change): ');
+        if (name !== '') {
+            user.name = name;
+            await user.save();
+            console.log('Display name updated');
+        }
+
+        console.log('');
+        let password = await cli.password('Enter new password (leave blank to not change): ');
+        if (password !== '') {
+            let confirm = await cli.password('Confirm new password: ');
+            if (password !== confirm) {
+                throw new Error('Passwords do not match');
+            }
+            user.password = await AuthUtils.hashPassword(password);
+            await user.save();
+            console.log('Password updated');
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+    cli.close();
+    await sequelize.close();
+});
