8a34525b779b5dedf409dafc40bf41b22b0ad8a2
Jean-Luc Herren <jlh@gmx.ch>
2021-08-20 22:16:26 +0200

    Backend: Add eventType table

---
+++ backend/migrations/2021-08-20 01 Add event type table.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const {DataTypes} = require('sequelize');
+
+/**
+ * @param {Sequelize} sequelize
+ * @returns {Promise<void>}
+ */
+async function up(sequelize) {
+    let queryInterface = sequelize.getQueryInterface();
+    let cascade = {
+        onDelete: 'restrict',
+        onUpdate: 'restrict',
+    };
+    let tableDefaultOptions = {
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci',
+    };
+    await queryInterface.createTable('eventType', {
+        id:        {
+            type:          DataTypes.TINYINT,
+            allowNull:     false,
+            primaryKey:    true,
+            autoIncrement: true,
+        },
+        createdAt: {
+            type:      DataTypes.DATE,
+            allowNull: false,
+        },
+        updatedAt: {
+            type:      DataTypes.DATE,
+            allowNull: false,
+        },
+    }, tableDefaultOptions);
+    let now = new Date();
+    await queryInterface.bulkInsert('eventType', [
+        {id: 1, createdAt: now, updatedAt: now},
+        {id: 2, createdAt: now, updatedAt: now},
+        {id: 3, createdAt: now, updatedAt: now},
+        {id: 4, createdAt: now, updatedAt: now},
+    ]);
+    await queryInterface.addIndex('event', {
+        name:   'event_type_idx',
+        fields: ['type'],
+    });
+    await queryInterface.addConstraint('event', {
+        type:       'foreign key',
+        name:       'event_ibfk_1',
+        fields:     ['type'],
+        references: {
+            table: 'eventType',
+            field: 'id',
+        },
+        ...cascade,
+    });
+}
+
+/**
+ * @param {Sequelize} sequelize
+ * @returns {Promise<void>}
+ */
+async function down(sequelize) {
+    let queryInterface = sequelize.getQueryInterface();
+    await queryInterface.removeConstraint('event', 'event_ibfk_1');
+    await queryInterface.removeIndex('event', 'event_type_idx');
+}
+
+module.exports = {up, down};
