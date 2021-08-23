4bc1810ac5879e5a5975cc848d21854abfaf3a5b
Jean-Luc Herren <jlh@gmx.ch>
2021-08-23 22:51:35 +0200

    Backend: Fix account tests

--- backend/tests/e2e/account.test.js
+++ backend/tests/e2e/account.test.js
@@ -120,13 +120,13 @@ describe('account check route', () => {
     it('works when not providing a token', async () => {
         let response = await request.get('/api/account/check');
         expect(response.status).toEqual(200);
-        expect(response.body).toEqual({});
+        expect(response.body).toEqual({userId: null, username: null});
     });
 
     it('works when providing a non-parsable token', async () => {
         let response = await request.get('/api/account/check').set('Authorization', 'Bearer WHATEVER');
         expect(response.status).toEqual(200);
-        expect(response.body).toEqual({});
+        expect(response.body).toEqual({userId: null, username: null});
     });
 
     it('works when providing an expired token', async () => {
@@ -135,7 +135,7 @@ describe('account check route', () => {
         let token = await user.generateToken(secret, {expiresIn: '-1 day'});
         let response = await request.get('/api/account/check').set('Authorization', `Bearer ${token}`);
         expect(response.status).toEqual(200);
-        expect(response.body).toEqual({});
+        expect(response.body).toEqual({userId: null, username: null});
     });
 
     it('works when providing a valid token', async () => {
