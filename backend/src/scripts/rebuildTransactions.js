b9b99ff4e5bd4fd45e3127433305da74e60069d3
Jean-Luc Herren <jlh@gmx.ch>
2022-02-04 17:24:36 +0100

    Add --dry-run mode for transaction rebuilder

--- backend/src/scripts/rebuildTransactions.js
+++ backend/src/scripts/rebuildTransactions.js
@@ -10,46 +10,56 @@ const ConfigProvider = require('../configProvider');
 /**
  * Rebuild all transactions
  *
+ * @param {boolean} doFix
+ *
  * @returns {Promise<void>}
  */
-async function rebuildTransactions() {
+async function rebuildTransactions(doFix) {
     console.log(chalk.bold('Rebuilding transactions and balances...'));
 
     let andeoLunch = new AndeoLunch({config: await ConfigProvider.getMainConfig()});
     await andeoLunch.waitReady();
     let sequelize = await andeoLunch.getSequelize();
+    let transaction = await sequelize.transaction();
 
     try {
-        await sequelize.transaction(async transaction => {
-            let events = await Models.Event.findAll({transaction});
-            let overallEarliestDate = null;
-            let nUpdatesTotal = 0;
+        let events = await Models.Event.findAll({transaction});
+        let overallEarliestDate = null;
+        let nUpdatesTotal = 0;
 
-            console.log(`Found ${events.length} events to rebuild`);
+        console.log(`Found ${events.length} events to rebuild`);
 
-            for (let event of events) {
-                console.log(`Rebuilding event ${event.id}`);
-                await Transaction.rebuildLunchDetails(transaction, event);
-                let {earliestDate, nUpdates} = await Transaction.rebuildEventTransactions(transaction, event);
-                if (earliestDate !== null && (overallEarliestDate === null || earliestDate < overallEarliestDate)) {
-                    overallEarliestDate = earliestDate;
-                }
-                nUpdatesTotal += nUpdates;
+        for (let event of events) {
+            console.log(`Rebuilding event ${event.id}`);
+            await Transaction.rebuildLunchDetails(transaction, event);
+            let {earliestDate, nUpdates} = await Transaction.rebuildEventTransactions(transaction, event);
+            if (earliestDate !== null && (overallEarliestDate === null || earliestDate < overallEarliestDate)) {
+                overallEarliestDate = earliestDate;
             }
+            nUpdatesTotal += nUpdates;
+        }
 
-            console.log(`Updated ${nUpdatesTotal} transactions`);
-            console.log('Rebuilding transaction balances');
-            let n = await Transaction.rebuildTransactionBalances(transaction, overallEarliestDate);
-            console.log(`Updated ${n} transaction balances`);
-            console.log('Rebuilding final balances');
-            await Transaction.rebuildUserBalances(transaction);
-        });
+        console.log(`Updated ${nUpdatesTotal} transactions`);
+        console.log('Rebuilding transaction balances');
+        let n = await Transaction.rebuildTransactionBalances(transaction, overallEarliestDate);
+        console.log(`Updated ${n} transaction balances`);
+        console.log('Rebuilding final balances');
+        await Transaction.rebuildUserBalances(transaction);
     } catch (err) {
-        console.error(err.message);
+        await transaction.rollback();
+        throw err;
+    }
+
+    if (doFix) {
+        await transaction.commit();
+        console.log(chalk.bold('Committing transaction...'));
+    } else {
+        await transaction.rollback();
+        console.log(chalk.yellow('Rolling back transaction due to --dry-run'));
     }
 
     await andeoLunch.close();
 }
 
 // noinspection JSIgnoredPromiseFromCall
-rebuildTransactions();
+rebuildTransactions(process.argv[2] !== '--dry-run');
