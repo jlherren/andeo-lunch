4c35ddead7125f4d342b9ea2299e80a345331edb
Jean-Luc Herren <jlh@gmx.ch>
2022-02-16 21:20:20 +0100

    Cypress: Fix tests with fixed user IDs

--- cypress/helpers/sql.js
+++ cypress/helpers/sql.js
@@ -4,8 +4,8 @@ export const USERS = {
         password: 'andeolunchtest',
         insert() {
             cy.task('db:sql', `
-                INSERT INTO user (id, username, password, name, active, createdAt, updatedAt)
-                    VALUES (2, 'john.doe', '$2a$10$EOq4EMCEzqoyWX.RezHdnuc/.oukv2lR2nVV1d8RyKVHMHwAq2/Wi', 'John Doe', 1, NOW(), NOW());
+                INSERT INTO user (username, password, name, active, createdAt, updatedAt)
+                    VALUES ('john.doe', '$2a$10$EOq4EMCEzqoyWX.RezHdnuc/.oukv2lR2nVV1d8RyKVHMHwAq2/Wi', 'John Doe', 1, NOW(), NOW());
             `);
         },
     },
@@ -15,8 +15,8 @@ export const USERS = {
         password: 'andeolunchtest',
         insert() {
             cy.task('db:sql', `
-                INSERT INTO user (id, username, password, name, active, createdAt, updatedAt)
-                    VALUES (3, 'robert.smith', '$2a$10$EOq4EMCEzqoyWX.RezHdnuc/.oukv2lR2nVV1d8RyKVHMHwAq2/Wi', 'Robert Smith', 1, NOW(), NOW());
+                INSERT INTO user (username, password, name, active, createdAt, updatedAt)
+                    VALUES ('robert.smith', '$2a$10$EOq4EMCEzqoyWX.RezHdnuc/.oukv2lR2nVV1d8RyKVHMHwAq2/Wi', 'Robert Smith', 1, NOW(), NOW());
             `);
         },
     },
