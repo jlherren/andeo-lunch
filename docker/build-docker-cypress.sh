503783294de76b1db987dc50c4d66c18b25356eb
Jean-Luc Herren <jlh@gmx.ch>
2022-02-21 19:53:21 +0100

    Cypress: Change port number used
    This avoids conflicts with ephemeral ports.

--- docker/build-docker-cypress.sh
+++ docker/build-docker-cypress.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-export VUE_APP_BACKEND_URL="http://localhost:53768/api"
+export VUE_APP_BACKEND_URL="http://localhost:31978/api"
 export VUE_APP_BRANDING_TITLE="Andeo Lunch (cypress)"
 export DOCKER_IMAGE_SUFFIX=-cypress
 
