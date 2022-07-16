652cd627ab098515b89c51aff1e66345f3d2a76d
Jean-Luc Herren <jlh@gmx.ch>
2022-07-16 16:11:24 +0200

    Cypress: Add test for password change

---
+++ cypress/e2e/profile.cy.js
@@ -0,0 +1,38 @@
+const {USERS} = require('../helpers/sql');
+
+describe('Profile', () => {
+    before(() => {
+        cy.task('db:purge');
+        USERS.john.insert();
+        cy.login(USERS.john.username, USERS.john.password);
+    });
+
+    it('Change password', () => {
+        const NEW_PASSWORD = 's3cr3t!';
+
+        cy.visit('/');
+        cy.get('.v-app-bar button')
+            .click();
+        cy.contains('a', 'Account settings')
+            .click();
+        cy.contains('a', 'Change password')
+            .click();
+        cy.followLabel('Current password')
+            .type(USERS.john.password);
+        cy.followLabel('New password')
+            .type(NEW_PASSWORD);
+        cy.followLabel('Confirm new password')
+            .type(NEW_PASSWORD);
+        cy.contains('button', 'Save')
+            .click();
+
+        cy.contains('[role=status]', 'Password changed successfully!')
+            .should('be.visible');
+
+        cy.contains('[role=listitem]', 'Logout')
+            .click();
+
+        cy.login(USERS.john.username, NEW_PASSWORD);
+        cy.contains('.v-app-bar', USERS.john.name);
+    });
+});
