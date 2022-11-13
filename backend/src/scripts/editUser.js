775952eb7c93d40ae04e68f7ad55168a07d04fd5
Jean-Luc Herren <jlh@gmx.ch>
2022-11-13 23:19:25 +0100

    Backend: Make user editor easier to use

--- backend/src/scripts/editUser.js
+++ backend/src/scripts/editUser.js
@@ -10,11 +10,99 @@ const ConfigProvider = require('../configProvider');
 const AuthUtils = require('../authUtils');
 
 /**
+ * @param {Cli} cli
+ * @param {User} user
+ * @returns {Promise<void>}
+ */
+async function editUser(cli, user) {
+    while (true) {
+        console.log('');
+        console.log('Editing user', user.username);
+        console.log('');
+        console.log('ID  Option');
+        console.log('1   Change display name');
+        console.log('2   Change password');
+        console.log('3   Activate/deactivate user');
+        console.log('x   Return to main menu');
+        console.log('');
+
+        let option = await cli.question('Select option ID: ');
+        console.log('');
+
+        if (option === '1') {
+            let name = await cli.question('New display name (leave blank to not change): ');
+            if (name !== '') {
+                user.name = name;
+                await user.save();
+                console.log('Display name updated');
+            }
+        } else if (option === '2') {
+            let password = await cli.password('Enter new password (leave blank to not change): ');
+            if (password !== '') {
+                let confirm = await cli.password('Confirm new password: ');
+                if (password !== confirm) {
+                    throw new Error('Passwords do not match');
+                }
+                user.password = await AuthUtils.hashPassword(password);
+                await user.save();
+                console.log('Password updated');
+            }
+        } else if (option === '3') {
+            user.active = !user.active;
+            await user.save();
+            console.log(`User ${user.username} is now ${user.active ? 'ACTIVE' : 'INACTIVE'}`);
+        } else if (option === 'x') {
+            break;
+        } else {
+            console.error('Invalid option');
+        }
+    }
+}
+
+/**
+ * @param {Cli} cli
+ * @returns {Promise<void>}
+ */
+async function mainMenu(cli) {
+    while (true) {
+        let users = await Models.User.findAll({order: [['id', 'ASC']]});
+        console.log('');
+        console.log('ID   Active  Username          Display name');
+        for (let user of users) {
+            console.log(sprintf('%-3d  %-3s     %-16s  %s', user.id, user.active ? 'Y' : 'N', user.username, user.name));
+        }
+        console.log('x    Exit');
+        console.log('');
+
+        let option = await cli.question('Select user ID: ');
+        console.log('');
+
+        if (option === 'x') {
+            break;
+        }
+
+        let userId = parseInt(option, 10);
+        if (isNaN(userId)) {
+            console.error('Invalid input');
+            continue;
+        }
+
+        let user = users.find(u => u.id === userId);
+        if (!user) {
+            console.error('No such user');
+            continue;
+        }
+
+        await editUser(cli, user);
+    }
+}
+
+/**
  * Edit user
  *
  * @returns {Promise<void>}
  */
-async function editUser() {
+async function main() {
     console.log(chalk.bold('Edit user'));
 
     let andeoLunch = new AndeoLunch({config: await ConfigProvider.getMainConfig()});
@@ -22,47 +110,7 @@ async function editUser() {
     let cli = new Cli();
 
     try {
-        let users = await Models.User.findAll({order: [['id', 'ASC']]});
-        console.log('\nID  Act  Username          Display name');
-        for (let user of users) {
-            console.log(sprintf('%-2d  %-3s  %-16s  %s', user.id, user.active ? 'Y' : 'N', user.username, user.name));
-        }
-        console.log('');
-
-        let userId = parseInt(await cli.question('Select user: '), 10);
-        let user = users.find(u => u.id === userId);
-
-        if (!user) {
-            throw new Error('Invalid user');
-        }
-
-        console.log('');
-        let name = await cli.question('New display name (leave blank to not change): ');
-        if (name !== '') {
-            user.name = name;
-            await user.save();
-            console.log('Display name updated');
-        }
-
-        console.log('');
-        let password = await cli.password('Enter new password (leave blank to not change): ');
-        if (password !== '') {
-            let confirm = await cli.password('Confirm new password: ');
-            if (password !== confirm) {
-                throw new Error('Passwords do not match');
-            }
-            user.password = await AuthUtils.hashPassword(password);
-            await user.save();
-            console.log('Password updated');
-        }
-
-        console.log('');
-        let prompt = user.active ? 'Deactivate user?' : 'Activate user?';
-        if (await cli.question(`${prompt} (y/N)`) === 'y') {
-            user.active = !user.active;
-            await user.save();
-            console.log('User updated');
-        }
+        await mainMenu(cli);
     } catch (err) {
         console.error(err.message);
     }
@@ -72,4 +120,4 @@ async function editUser() {
 }
 
 // noinspection JSIgnoredPromiseFromCall
-editUser();
+main();
