d10d7aa30594ed12ae9f81d95a024001cf91f1af
Jean-Luc Herren <jlh@gmx.ch>
2022-11-30 22:06:34 +0100

    App: Disable money factor when opt-out

--- app/src/components/event/ParticipationEdit.vue
+++ app/src/components/event/ParticipationEdit.vue
@@ -25,7 +25,7 @@
                         <v-text-field v-model="moneyCredited" type="number" min="0"
                                       label="Money credited" :append-icon="$icons.money"/>
                         <number-field v-model="moneyFactor" :min="0" :step="5" v-if="event.type === 'special'"
-                                      label="Money factor" suffix="%" :icon="$icons.money"/>
+                                      label="Money factor" suffix="%" :icon="$icons.money" :disabled="type === 'opt-out'"/>
                     </v-col>
                 </v-row>
 
