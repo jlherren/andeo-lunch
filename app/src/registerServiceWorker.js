8bcc961fe11ea5ec6b5cee43906ad452e5700513
Jean-Luc Herren <jlh@gmx.ch>
2022-12-03 17:14:23 +0100

    Cypress: Do not register the service worker
    This previously prevented Cypress from working at all with Electron, and in
    Chromium it would test an old cached copy of the app.

--- app/src/registerServiceWorker.js
+++ app/src/registerServiceWorker.js
@@ -3,7 +3,11 @@
 import {EventService} from '@/services/eventService';
 import {register} from 'register-service-worker';
 
-if (process.env.NODE_ENV === 'production') {
+// Disable registering the service worker when running in Cypress, because this causes a lot of issues, where old
+// version of the app will be tested instead of the current one.  Also see:
+// https://github.com/cypress-io/cypress/issues/702
+
+if (process.env.NODE_ENV === 'production' && !window.Cypress) {
     register(`${process.env.BASE_URL}service-worker.js`, {
         offline() {
             console.log('No internet connection found. App is running in offline mode.');
