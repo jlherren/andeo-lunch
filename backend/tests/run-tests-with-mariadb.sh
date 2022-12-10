374df5abe0ace1155101c87c80b3e79e034259c9
Jean-Luc Herren <jlh@gmx.ch>
2022-12-10 16:13:50 +0100

    Backend: Run tests on more version of MariaDB

--- backend/tests/run-tests-with-mariadb.sh
+++ backend/tests/run-tests-with-mariadb.sh
@@ -15,7 +15,7 @@ cd $(dirname "$0")/..
 
 # MariaDB 10.5 is mostly used for development, 10.6 is used for production.
 
-for MARIADB_VERSION in 10.5 10.6 10.7; do
+for MARIADB_VERSION in 10.5 10.6 10.7 10.8 10.9 10.10; do
     echo "Removing existing container"
     docker container rm --force "$DB_CONTAINER_NAME" 2> /dev/null || true
 
